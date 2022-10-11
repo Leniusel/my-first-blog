5618e4b09f2f83e20d508329bffbef599c19d879
Leniusel <112113603+Leniusel@users.noreply.github.com>
2022-10-10 17:16:38 -0700

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('', include('blog.urls')),
     path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
 ]
