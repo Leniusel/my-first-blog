e09e468b6d6cfd4e1067e93bc542adad94d55334
Leniusel <112113603+Leniusel@users.noreply.github.com>
2022-10-10 17:11:08 -0700

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/login/', views.LoginView.as_view(), name='login'),
     path('', include('blog.urls')),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
 ]
